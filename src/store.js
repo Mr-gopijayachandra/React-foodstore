ca371373d7f0eaeb73f8e45dae46acc2cac43d45
chanduj <jay.gayala2002@gmail.com>
2025-02-12 12:46:17 +0530

    mod

--- src/store.js
+++ src/store.js
@@ -63,25 +63,25 @@ const productSlice = createSlice({
     initialState: {
         veg: [
             { name: 'tomato', price: 90.05, image: "https://upload.wikimedia.org/wikipedia/commons/8/89/Tomato_je.jpg" },
-            { name: 'potato', price: 250.07, image: "potato.jpg" },
-            { name: 'carrot', price: 300.05, image: "carrot.jpg" },
-            { name: 'chilli', price: 10.05, image: "chilli.jpg" },
-            { name: 'ladys finger', price: 60.05, image: "lady.png" },
-            { name: 'cabbage', price: 80.05, image: "cab.png" },
-            { name: 'cauliflower', price: 100.05, image: "cali.png" },
-            { name: 'beans', price: 40.05, image: "beans.png" },
+            { name: 'potato', price: 250.07, image: "/potato.jpg" },
+            { name: 'carrot', price: 300.05, image: "/carrot.jpg" },
+            { name: 'chilli', price: 10.05, image: "/chilli.jpg" },
+            { name: 'ladys finger', price: 60.05, image: "/lady.png" },
+            { name: 'cabbage', price: 80.05, image: "/cab.png" },
+            { name: 'cauliflower', price: 100.05, image: "/cali.png" },
+            { name: 'beans', price: 40.05, image: "/beans.png" },
             
 
         ],
         nonveg: [
-            { name: 'chicken', price: 300.05, image: "chick.jpg" },
-            { name: 'mutton', price: 850.07, image: "mutton.jpg" },
-            { name: 'fish', price: 1000.05, image: "fish.jpg" },
-            { name: 'prawns', price: 900.05, image: "prw.png" },
-            { name: 'crabe', price: 800.05, image: "crab.png" },
-            { name: 'lobster', price: 950.05, image: "lob.png" },
-            { name: 'apollo fish', price: 1150.05, image: "apoll.jpg" },
-            { name: 'leg pices', price: 500.05, image: "leg.jpg" },
+            { name: 'chicken', price: 300.05, image: "/chick.jpg" },
+            { name: 'mutton', price: 850.07, image: "/mutton.jpg" },
+            { name: 'fish', price: 1000.05, image: "/fish.jpg" },
+            { name: 'prawns', price: 900.05, image: "/prw.png" },
+            { name: 'crabe', price: 800.05, image: "/crab.png" },
+            { name: 'lobster', price: 950.05, image: "/lob.png" },
+            { name: 'apollo fish', price: 1150.05, image: "/apoll.jpg" },
+            { name: 'leg pices', price: 500.05, image: "/leg.jpg" },
             
         ]
     },
